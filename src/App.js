d92e91a9a354089f75fb21e2e1081ee50bf860ab
SivaranjiniSanthanakrishnan <43466009+SivaranjiniSanthanakrishnan@users.noreply.github.com>
2022-09-19 12:51:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Article from "./Components/article";
+
+function App() {
+  return (
+    <div style={{ padding: "30px" }}>
+      <Article />
+    </div>
+  );
+}
+
+export default App;
